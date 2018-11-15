23763e4c381ef5c4621e265d748232358455195a
susanne streety <s.streety@appliedmemeticsllc.com>
2018-11-15 16:27:14 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
